2b17a53a3f2fd623e2262b889bc6c053709e2182
paul kairu <paulkairukamau@gmail.com>
2024-10-07 12:04:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+window.bestCustomer
+const leastFavoriteCustomer = 'not bob';
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+  }
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'something else'; 
+  }
